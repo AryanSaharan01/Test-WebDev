252d5d9073f20c0be234fd75e2ffa478ec159616
Aryan Saharan <124769240+AryanSaharan01@users.noreply.github.com>
2024-01-21 18:37:12 +0530

    new

---
+++ JS_variables.js
@@ -0,0 +1,39 @@
+console.log("Heyy there this is TEXT");
+
+// We'll not use var in JS
+// var a = 5;
+// var b = 12;
+// var c = "Aryan Saharan"
+
+// console.log(a+b)
+// console.log (typeof a, typeof b, typeof c)
+
+// const a1 = 8
+
+let a = 5;
+a = a + 3;
+let b = 12;
+console.log(a);
+
+{
+  let a = 45;
+  console.log(a);
+}
+
+let x = "Aryan";
+let y = 12;
+let z = 5.456;
+const p = true;
+let q = undefined;
+let r = null;
+
+console.log(x, y, z, p, q, r);
+console.log(typeof x, typeof y, typeof z, typeof p, typeof q, typeof r);
+
+let o = {
+  name: "Aryan Saharan",
+  "job code": 2356,
+};
+console.log(o);
+o.salary = "100 cr";
+console.log(o);
---
+++ conditioal.js
@@ -0,0 +1,47 @@
+console.log(" #-#-#-#-BUDDY-#-#-#-# ")
+// console.log("Heyy there this is TEXT");
+
+let age = 12;
+let grace = 1;
+
+age += grace
+console.log(age)
+console.log(age + grace)
+console.log(age - grace)
+console.log(age * grace)
+console.log(age / grace)
+console.log(age ** grace)
+console.log(age % grace)
+
+
+
+if (age>20) {
+console.log("Heyy! you can drive...")
+}
+
+else{
+    console.log("YOU can't")
+}
+
+// ---#---#---#---#---#---#---#---#---#---#---#---#---#---#---#---#---#---#---#---#---#---#---#---#---#---#---#
+
+if (age == 18) {
+    console.log("You can drive");
+}
+
+else if (age == 0) {
+    console.log("Are you kidding?")
+}
+
+else if (age == 1) {
+    console.log("Are you again kidding?")
+}
+
+else {
+    console.log("You cannot LAUGH");
+}
+
+// ---#---#---#---#---#---#---#---#---#---#---#---#---#---#---#---#---#---#---#---#---#---#---#---#---#---#---#---#
+
+
+
